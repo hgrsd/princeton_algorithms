e67589105fd7e3d5c30ead8925ae8611353aa490
Daniel <daniel@hgrsd.nl>
2019-03-14 16:51:12 +0000

    Pipenv initialised. Started point.py for the second part of the assignment.

--- week 2/permutation.py
+++ week 2/permutation.py
@@ -2,13 +2,13 @@ import sys
 import randomised_queue
 
 """
-Part of week 2's assignment of the Princeton Algorithm (Part I) course on Coursera, 
-see http://coursera.cs.princeton.edu/algs4/assignments/queues.html. 
+Part of week 2's assignment of the Princeton Algorithm (Part I) course on Coursera,
+see http://coursera.cs.princeton.edu/algs4/assignments/queues.html.
 
 To be used as a driver for randomised_queue.py.
 
-This programme reads in a number of strings and, after user input stops, prints out a number of 
-strings at random, dequeue-ing them from the randomised queue. 
+This programme reads in a number of strings and, after user input stops, prints out a number of
+strings at random, dequeue-ing them from the randomised queue.
 """
 
 
