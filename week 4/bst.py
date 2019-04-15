ec95cb825e535bcfe972f9949cba13e443afdced
Daniel <daniel@hgrsd.nl>
2019-04-15 21:54:48 +0100

    Renamed methods. Added rank method.

--- week 4/bst.py
+++ week 4/bst.py
@@ -7,7 +7,7 @@ class BST:
         return BST.size(self.root)
 
     def put(self, key, value):
-        self.root = self._put(self.root, key, value)
+        self.root = self.put_recurse(self.root, key, value)
 
     def get(self, key):
         scan = self.root
@@ -21,10 +21,10 @@ class BST:
         raise IndexError("Key not found.")
 
     def delete(self, key):
-        self.root = BST._delete(self.root, key)
+        self.root = BST.delete_recurse(self.root, key)
 
     def del_min(self):
-        self.root = BST._del_min(self.root)
+        self.root = BST.del_min_recurse(self.root)
 
     def generate_tree(self):
         if len(self) == 0:
@@ -42,43 +42,46 @@ class BST:
             tree.append(cur)
         return tree
 
+    def rank(self, key):
+        return BST.rank_recurse(self.root, key)
+
     @staticmethod
-    def _put(scan, key, value):
+    def put_recurse(scan, key, value):
         if not scan:
             return Node(key, value)
         if key < scan.key:
-            scan.left = self._put(scan.left, key, value)
+            scan.left = BST.put_recurse(scan.left, key, value)
         elif key > scan.key:
-            scan.right = self._put(scan.right, key, value)
+            scan.right = BST.put_recurse(scan.right, key, value)
         else:
             scan.value = value
         scan.size = 1 + BST.size(scan.left) + BST.size(scan.right)
         return scan
 
     @staticmethod
-    def _del_min(scan):
+    def del_min_recurse(scan):
         if not scan.left:
             return scan.right
-        scan.left = BST._del_min(scan.left)
+        scan.left = BST.del_min_recurse(scan.left)
         scan.size = 1 + BST.size(scan.left) + BST.size(scan.right)
         return scan
     
     @staticmethod
-    def _delete(scan, key):
+    def delete_recurse(scan, key):
         if not scan:
             return None
         if key < scan.key:
-            scan.left = BST._delete(scan.left, key)
+            scan.left = BST.delete_recurse(scan.left, key)
         elif key > scan.key:
-            scan.right = BST._delete(scan.right, key)
+            scan.right = BST.delete_recurse(scan.right, key)
         else:
             if not scan.right:
                 return scan.left
             if not scan.left:
                 return scan.right
             temp = scan
-            scan = BST._min(temp.right)
-            scan.right = BST._del_min(temp.right)
+            scan = BST.min(temp.right)
+            scan.right = BST.del_min_recurse(temp.right)
             scan.left = temp.left
         scan.size = 1 + BST.size(scan.left) + BST.size(scan.right)
         return scan
@@ -94,6 +97,17 @@ class BST:
         while scan.right:
             scan = scan.right
         return scan
+
+    @staticmethod
+    def rank_recurse(scan, key):
+        if not scan:
+            return 0
+        if key < scan.key:
+            return BST.rank_recurse(scan.left, key)
+        elif key > scan.key:
+            return 1 + BST.size(scan.left) + BST.rank_recurse(scan.right, key)
+        else:
+            return BST.size(scan.left)
 
     @staticmethod
     def size(node):
